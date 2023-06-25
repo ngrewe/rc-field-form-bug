3763d7ad92d40b395371e9c63b256153a380d472
Niels Grewe <niels@zasta.de>
2023-06-25 11:12:44 +0200

    chore: wording

--- src/Bug.tsx
+++ src/Bug.tsx
@@ -7,7 +7,7 @@ export const Bug: FC = () => (
   <>
   <p>
     The following rc-field-form is set up with an <code>action</code> and <code>method</code> property. The expected behaviour
-    is that the form element is submitted to the server as expected. But doesn't do that, because rc-field-form <a href="https://github.com/react-component/field-form/blob/04b1667b32013338ad32c09895a5b84714c5d589/src/Form.tsx#L164-L165">prevents event propagation</a>.
+    is that the form data is submitted to the endpoint mentioned in <code>action</code>. But it doesn't do that, because rc-field-form <a href="https://github.com/react-component/field-form/blob/04b1667b32013338ad32c09895a5b84714c5d589/src/Form.tsx#L164-L165">prevents event propagation</a>.
   </p>
     <Form name="test-form" action="https://httpstat.us/200" method="post">
       <Field name="test-field">
